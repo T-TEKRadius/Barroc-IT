95e31b49f00f24929b450901e012c6a7a32a750b
Unknown <d255807@edu.rocwb.nl>
2018-10-04 13:14:55 +0200

    Added Main Structure

---
+++ laravel/resources/views/development/development.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Barroc-IT | Development</title>
+    <link href="{{ asset('/css/css.css') }}" rel="stylesheet"> 
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>Barroc-IT | Development</h1>
+        </div>
+    </header>
+    <section id="main">
+        <div class="container">
+            <h2>Clients</h2>
+            <div class="search">
+                <input type="text" name="search" placeholder="Search..">
+            </div>
+            <div class="logout">
+                <button type="submit">Logout</button>
+            </div>
+            <div class="display">
+                <h3>Client</h3>
+                <h3>Status</h3>
+                <h3>Finished</h3>
+            </div>
+        </div>
+    </section>
+    <section id="help">
+        <div class="container">
+            <h2>Help</h2>
+        </div>
+    </section>
+</body>
+
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/development', function () {
+    return view('development/development');
+});
+
+Route::get('/sales', function (){
+    return view('sales/sales');
+});
+
+Route::get('/sales/create', function (){
+    return view('sales/create');
+});
+
+Route::get('sales/edit', function (){
+    return view('sales/edit');
+});
+
+Route::get('/sales/status', function (){
+    return view('sales/status');
+});
+
+Route::get('/finance/status', function (){
+    return view('finance/status');
+});
