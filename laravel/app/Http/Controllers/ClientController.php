a9bfc0f50f5fd73ef1f37f02f511135bd8ed3b09
Unknown <d255807@edu.rocwb.nl>
2018-10-05 11:31:27 +0200

    Fixed Bug

--- laravel/app/Http/Controllers/ClientController.php
+++ laravel/app/Http/Controllers/ClientController.php
@@ -19,38 +19,36 @@ class ClientController extends Controller
         return view('sales/status')
             ->with('clients', $clients);
     }
-    public function create(Request $request){
+    public function create(Request $request)
+    {
 
         $input = $request->all();
         $exist = Client::where('company_name', $input['company_name'])->first();
-        if($exist){
+        if ($exist) {
             echo 'Already Exists';
-        }
+        } elseif (!$exist) {
             $inputs = [
-        elseif (!$exist){
-                'address_1'=> $input['address_1'],
-                'company_name'=> $input['company_name'],
-                'zipcode_1'=> $input['zipcode_1'],
-                'residence_1'=> $input['residence_1'],
-                'zipcode_2'=> $input['zipcode_2'],
-                'address_2'=> $input['address_2'],
-                'contact_person'=> $input['contact_person'],
-                'residence_2'=> $input['residence_2'],
-                'initials'=> $input['initials'],
-                'phone_number'=> $input['phone_number'],
-                'fax_number'=> $input['fax_number'],
-                'email'=> $input['email'],
-                'discount_percentage'=> $input['sales_percentage'],
-                'credit_worthy'=> $input['credit_worthy'],
-                'prospect'=> $input['prospect']
+                'address_1' => $input['address_1'],
+                'company_name' => $input['company_name'],
+                'zipcode_1' => $input['zipcode_1'],
+                'residence_1' => $input['residence_1'],
+                'zipcode_2' => $input['zipcode_2'],
+                'address_2' => $input['address_2'],
+                'contact_person' => $input['contact_person'],
+                'residence_2' => $input['residence_2'],
+                'initials' => $input['initials'],
+                'phone_number' => $input['phone_number'],
+                'fax_number' => $input['fax_number'],
+                'email' => $input['email'],
+                'discount_percentage' => $input['sales_percentage'],
+                'credit_worthy' => $input['credit_worthy'],
+                'prospect' => $input['prospect']
             ];
             Client::create($inputs);
             echo 'Client Added';
+            return redirect('sales/status');
         }
-
-        return view('sales/sales');
-    public function edit(){
     }
-
+    public function edit(){
     }
 }
--- laravel/resources/views/sales/status.blade.php
+++ laravel/resources/views/sales/status.blade.php
@@ -23,7 +23,7 @@
             </div>
             <div class="buttons">
               <a class="logout" href="#">Logout</a>
-                <a class="add" href="#">Add client</a>
+                <a class="add" href="create">Add client</a>
                 </div>
                 </div>
             <div class="display">
