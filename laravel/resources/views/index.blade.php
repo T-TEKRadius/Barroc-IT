4bb9fc214f2deb947b4ab6b08f3ab7bcb71fc3be
Kevin Musters <kevinmusters@hotmail.com>
2018-10-09 10:06:15 +0200

    Edit login page html and styling

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -1,28 +1,38 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
     <title>Barroc-IT</title>
-    <link href="{{ asset('/css/css.css') }}" rel="stylesheet"> 
-  </head>
-  <body>
+    <link href="{{ asset('/css/css.css') }}" rel="stylesheet">
+</head>
+<body>
 <header>
-  <div class="container">
-    <h1>Barroc-IT</h1>
-  </div>
-  </header>
-  <div class="container">
-    <section id="login">
-    <form>
-      <label for="psw">Password</label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-    <button type="submit">Login</button>
-  </div>
-  </section>
-  <div class="container">
-    <section id="help">
-    <h2>Help</h2>
+    <div class="container">
+        <h1>Barroc-IT</h1>
     </div>
-  </section>
-  </body>
+</header>
+<main class="login">
+    <div class="container">
+        <section id="login">
+            <h2>Login Barroc-IT</h2>
+            <form>
+                <div>
+                    <label for="psw">Password</label>
+                    <input type="password" placeholder="Enter Password" name="psw" required>
+                </div>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </section>
+    </div>
+</main>
+<footer>
+    <div class="container">
+        <section id="help">
+            <h2>Help</h2>
+        </section>
+    </div>
+</footer>
+</body>
 </html>
